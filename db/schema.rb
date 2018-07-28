ef4beb1c15af90e17c2341ba1709da102cf39019
aliciapflaumer <alpfla7@gmail.com>
2018-07-28 14:52:55 -0400

    Created Users
    Used `rails g scaffold Users username:string password_digest`

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_28_151357) do
+ActiveRecord::Schema.define(version: 2018_07_28_153824) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "item"
     t.string "note"
     t.boolean "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
