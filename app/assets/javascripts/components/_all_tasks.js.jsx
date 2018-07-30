6a1f34e63368f396e9da50ca626869f005bd7ae8
aliciapflaumer <alpfla7@gmail.com>
2018-07-30 15:18:58 -0400

    Created second Component with React
    - Created new file `_all_tasks` and defined the new Component `AllTasks`
    - Placed the Component inside the main component with `<AllTasks />`

---
+++ app/assets/javascripts/components/_all_tasks.js.jsx
@@ -0,0 +1,9 @@
+class AllTasks extends React.Component {
+render(){
+  return(
+    <div>
+      <h2>To do: list of tasks</h2>
+    </div>
+    )
+  }
+}
--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -2,7 +2,8 @@ const Main = (props) => {
     return(
       <div>
         <h1>Taskr app</h1>
-        <p>React frontend and Rails backend</p>
+        <p><i>React frontend and Rails backend</i></p>
+        <AllTasks />
       </div>
     )
 }
